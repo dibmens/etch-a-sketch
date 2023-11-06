df216bbba30d5f6fb3bb9441259803d08a4cb952
dibmens <dibmens@gmail.com>
2023-11-06 21:37:56 +0200

    Create the grid and add basic styling

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+let container = document.querySelector('#container');
+
+for(i = 0; i < 256; i++ ){
+    let cell = document.createElement('div');
+    container.append(cell);
+    cell.className = 'cell';
+}
 
