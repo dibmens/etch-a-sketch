ff2f44e62640536c64d81823bb995878047ec932
dibmens <dibmens@gmail.com>
2023-11-06 22:53:40 +0200

    Add a grid spawning function (incomplete)

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 let container = document.querySelector('#container');
+let button = document.querySelector('#btn');
 
-for(i = 0; i < 256; i++ ){
+function spawnGrid(size = 16){
+
+    for(i = 0; i < size*size; i++ ){
     let cell = document.createElement('div');
     container.append(cell);
     cell.className = 'cell';
+    }
 }
 
+spawnGrid();
+
+let grid = document.querySelectorAll('.cell');
+
+// container.addEventListener('mouseenter', ()=> container.classList.add('ink'));
+
+button.addEventListener('click', ()=>{
+
+    let oldGrid = document.querySelectorAll('.cell');
+    grid.remove();
+
+    let size = prompt('Enter your preferred grid size (4x4-20x20)',16);
+    spawnGrid(size);
+})
